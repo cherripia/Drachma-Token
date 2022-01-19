bd576d31eff2ccafa4e05df1c76f5f0e8872bc7f
cherripia <cherririri@protonmail.ch>
2022-01-19 12:22:18 -0500

    Ownable

--- contracts/DrachmaToken.sol
+++ contracts/DrachmaToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract DrachmaToken is ERC20, ERC20Burnable{
+contract DrachmaToken is ERC20, ERC20Burnable, Ownable{
 
     using SafeMath for uint256;
 

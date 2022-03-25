64f99258e30ab76ac2038716a80953ba2df7820e
cherripia <cherririri@protonmail.ch>
2022-03-25 03:54:02 -0400

    func

--- contracts/DrachmaToken.sol
+++ contracts/DrachmaToken.sol
@@ -6,32 +6,41 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-// Dream Scenario;
-// Burn Fee: 2% (Every transaction)
-// Charity Fee: 2% ETH (Every exchange transaction; Only ETH.)
-// Liquidity Fee: 1% ETH (Every exchange transaction. Only ETH. Add to liquidity.)
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 
-contract DrachmaToken is ERC20, ERC20Burnable, Ownable{
+contract DrachmaToken is ERC20, ERC20Burnable, Ownable {
 
     using SafeMath for uint256;
 
     mapping(address => bool) private _excludeFromFees;
-    uint256 CharityFee = 2; // Percentage of transactions moved to charity wallet.
-    uint256 BurnFee = 2; // Percentage of transactions removed every transaction.
+
+    address private _charityWallet = address(0x0D75c77DF0De81399C921B65262d9Ed4b23D1236);
+    address private _burnWallet = address(0xF4113a6e003061Ae82D0c12Cb948C5cfcb3aE987);
+
+    uint256 _developerFee = 2; // Percentage of trades on exchanges to developer wallets.
+    uint256 _burnAndCharityFee = 2; // Percentage of transactions burned (DRC) and donated (ETH) every transaction.
+    uint256 _liquidityFee = 1; // Liquidity Fee 
 
     constructor(uint256 _supply) ERC20("Drachma", "DRC") {
-        _mint(msg.sender, _supply * (10 ** decimals()));
-        _excludeFromFees[msg.sender] = true;
+        _mint(_msgSender(), _supply * (10 ** decimals()));
+        _excludeFromFees[_msgSender()] = true;
     }
 
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        if (_excludeFromFees[_msgSender()]) {
+        if ((_excludeFromFees[_msgSender()]) || (_excludeFromFees[recipient])) {
             _transfer(_msgSender(), recipient, amount);
         } else {
 
-            // Burn 2% on ALL transactions.
-            uint256 burnAmount = amount.mul(BurnFee).div(100);
-            _burn(_msgSender(), burnAmount);
+            /*
+            // Burn 2% to charity wallet.
+            uint256 charityAmount = amount.mul(_charityFee).div(100);
+            _transfer(_msgSender(), _charityWallet, amount.sub(charityAmount));
+            */
+
+            // Burn 2% on all transactions.
+            uint256 burnAmount = amount.mul(_burnFee).div(100);
+            //_burn(_msgSender(), burnAmount);
+            _transfer(_msgSender(), _burnWallet, amount.sub(burnAmount));
 
             _transfer(_msgSender(), recipient, amount.sub(burnAmount));
         }
--- test/DrachmaToken.test.js
+++ test/DrachmaToken.test.js
@@ -7,31 +7,34 @@ contract("DrachmaToken", (accounts) => {
         console.log("Drachma Contract Address: " + drachmatoken.address);
     })
 
-    it("Gives the contract creator 1T tokens", async () => {
+    it("Gives the contract creator role of owner and 1T DRC", async () => {
         let balance = await drachmatoken.balanceOf(accounts[0]);
         console.log("Accounts[0] balance (in wei): " + balance);
         balance = web3.utils.fromWei(balance, "ether");
         console.log("Accounts[0] balance: " + balance);
-        assert.equal(balance, "1000000000000", "Balance should be 1T tokens for contract creator.")
+        assert.equal(balance, "1000000000000", "Balance should be 1T DRC for contract creator.")
+        let owner = await drachmatoken.owner();
+        console.log(owner);
+        assert.equal(owner, accounts[0], "Accounts[0] should be the owner of the contract.");
     })
 
     it("Excludes contract creator from transfer fees", async () => {
         let amount =  web3.utils.toWei("100", "ether")
-        console.log("Transfering " + amount + " tokens.");
+        console.log("Transfering " + amount + " DRC.");
         await drachmatoken.transfer(accounts[1], amount, {from: accounts[0]});
         let balance = await drachmatoken.balanceOf(accounts[1]);
         balance = web3.utils.fromWei(balance, "ether");
-        console.log("Accounts[1] balance after transfer: " + balance);
+        console.log("Accounts[1] DRC after transfer: " + balance);
         assert.equal(balance, 100, "Reciever balance should 100.")
     })
 
-    it("Burns 2% of tokens between normal account transfers", async () => {
+    it("Burns 2% of DRC between normal account transfers", async () => {
         let amount =  web3.utils.toWei("100", "ether")
         await drachmatoken.transfer(accounts[2], amount, {from: accounts[1]});
         let balance = await drachmatoken.balanceOf(accounts[2]);
         balance = web3.utils.fromWei(balance, "ether");
-        console.log("Accounts[2] balance after transfer: " + balance);
-        assert.equal(balance, "98", "Reciever balance should be 98.")
+        console.log("Accounts[2] DRC after transfer: " + balance);
+        assert.equal(balance, "98", "Reciever DRC should be 98.")
     })
 
 })
